59cc1de2827dd94e59cf05f547096178a7021e7f
eespejo1987 <eespejo1987@gmail.com>
2021-11-22 22:24:08 -0300

    se agregan modificaciones heroku

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 const express = require('express')
 const path = require ('path') // para agregar html
 const app = express()
-const port = 3030
+/*const port = 3030*/
 app.use(express.static('public')); // imagenes + css
  
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
 
-app.listen(port , ()=>{
+app.listen(process.env.PORT|| 3000 , ()=>{
     console.log('Example app listening at http://localhost:${port}')
 })
